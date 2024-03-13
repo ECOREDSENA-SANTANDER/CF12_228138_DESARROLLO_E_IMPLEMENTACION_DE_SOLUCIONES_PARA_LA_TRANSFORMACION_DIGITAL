d6e6b496e452b95997baf1e1764dcd665351a574
PABG1990 <110750475+PABG1990@users.noreply.github.com>
2024-03-13 12:14:02 -0500

    entrega

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -7,15 +7,15 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/template/Completar.svg", alt="Texto que describa la imagen")
+            img(src="@/assets/template/falso-verdadero.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
-          h2 Conceptos de computación en la nube y adopción de tecnologías disponibles. 
-        p.mb-4 #[b Identificar conceptos relacionados con la computación en la nube con el fin de  implementarla en las organizaciones. ] 
+          h2 Nombre de la actividad
+        p.mb-4 #[b Objetivo] 
         .tarjeta.actividad.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 COMPLETAR ESPACIOS
+              p.fw-bold.mb-0 Tipo de actividad
             .col-auto
               a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
                 span Realizar
--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -2,28 +2,19 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    .titulo-sexto.colo-acento-contenido.mb-4(data-aos="zoom-in-left")
-      h5 Desarrollo e Implementación de Soluciones para la Transformación Digital
-        span
-          | Migración de servicios en la nube.
-    figure.mt-4.mb-5(data-aos="zoom-in-left")
+    p.mb-5 A continuación, se muestra un mapa conceptual con los elementos más importantes desarrollados en este componente.
 
-  
-        .col-12.my-3
-          p.mb-3 Los servicios en la  nube son infraestructuras, plataformas o sistemas de software que alojan los proveedores externos y que se ponen a disposición de los usuarios a través de Internet. Facilitan el flujo de datos de los usuarios a través de Internet, referenciando lo anterior la migración de los diferentes servicios requiere de diferentes formas de almacenamiento, metodologías o estrategias de migración y monitorización. 
-          p.mb-0 A continuación, se presenta el diagrama que representa el resumen de las temáticas que están desarrolladas en el componente formativo: 
-    
     .row.justify-content-center
       .col-lg-10.mb-5
         figure
-          img(src="@/assets/curso/temas/sintesis.png", alt="alt")
+          img(src="@/assets/curso/temas/sintesis.svg", alt="alt")
       .col-auto
         a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
             img(src="@/assets/template/icono-pdf.svg")
           .anexo__texto
             p Anexo. Síntesis
-    
+
 </template>
 
 <script>
