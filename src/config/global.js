121bd6bfd4440d00b46a52faa957917ceed9fd92
PABG1990 <110750475+PABG1990@users.noreply.github.com>
2024-07-25 21:26:52 -0500

    Entrega Final

--- src/config/global.js
+++ src/config/global.js
@@ -173,7 +173,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/CF12_228138_DU.pdf',
+        download: 'downloads/CF12_228138_DU.zip',
       },
       {
         icono: 'fas fa-download',
